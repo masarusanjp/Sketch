8abdc0074349e95647142679affa7c82eca52917
masaichi <masarusanplusplus@gmail.com>
2016-01-28 23:04:48 +0900

    Added dash method

--- Pod/Classes/Sketch.swift
+++ Pod/Classes/Sketch.swift
@@ -28,15 +28,27 @@ public class Sketch {
                 CGContextSetStrokeColorWithColor(context, color.CGColor)
             }
         }
-        
+
         public func lineWidth(width: Double) {
             CGContextSetLineWidth(context, CGFloat(width))
+        }
+
+        public func lineDash(dashWidth: Double) {
+            let lengths = [CGFloat(dashWidth)]
+            CGContextSetLineCap(context, CGLineCap.Round)
+            CGContextSetLineDash(context, CGFloat(0.0), lengths, lengths.count)
         }
 
         public func circle(r: Float) {
             let radius = CGFloat(r)
             CGContextAddArc(context, radius, radius, radius, 0, CGFloat(2 * M_PI), 0)
             CGContextFillPath(context)
+            CGContextStrokePath(context)
+        }
+
+        public func line(x1: Double, y1: Double, x2: Double, y2: Double) {
+            let points = [CGPoint(x: x1, y: y1), CGPoint(x: x2, y: y2)]
+            CGContextAddLines(context, points, points.count)
             CGContextStrokePath(context)
         }
     }
