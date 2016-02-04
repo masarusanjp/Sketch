8c860ac5f5dad9d76835d9977afb7a85fa63fb35
masaichi <masarusanplusplus@gmail.com>
2016-02-04 23:01:35 +0900

    added gradient func

--- Pod/Classes/Sketch.swift
+++ Pod/Classes/Sketch.swift
@@ -11,14 +11,73 @@ import UIKit
 import QuartzCore
 
 public class Sketch {
+    public enum SketchError : ErrorType {
+        case ResourceCreationFail(functionName: String)
+    }
+    public enum ColorCode {
+        case RGBA(Double, Double, Double, Double)
+        case RGB(Double, Double, Double)
+        case GrayScale(white:Double, alpha: Double)
+
+        func asCGFloatArray() -> [CGFloat] {
+            switch (self) {
+            case let .RGBA(r, g, b, a):
+                return [CGFloat(r), CGFloat(g), CGFloat(b), CGFloat(a)]
+            case let .RGB(r, g, b):
+                return [CGFloat(r), CGFloat(g), CGFloat(b), 1.0]
+            case let .GrayScale(white: w, alpha: a):
+                return [CGFloat(w), CGFloat(w), CGFloat(w), CGFloat(a)]
+            }
+        }
+    }
+
+    public struct Gradient {
+        let locations: [CGFloat]
+        let colors: [ColorCode]
+
+        func createGradient() throws -> CGGradientRef {
+            let colorSpace = try createColorSpace()
+            let components = colors.map { (c) in
+                return c.asCGFloatArray()
+            }
+
+            if let gradient = CGGradientCreateWithColors(colorSpace, components, locations) {
+                return gradient
+            } else {
+                throw SketchError.ResourceCreationFail(functionName: "CGGradientCreateWithColors")
+            }
+        }
+
+        private func createColorSpace() throws -> CGColorSpaceRef {
+            if let colorSpace = CGColorSpaceCreateDeviceRGB() {
+                return colorSpace
+            } else {
+                throw SketchError.ResourceCreationFail(functionName: "CGColorSpaceCreateDeviceRGB")
+            }
+        }
+    }
+
     public class Canvas {
+
         public let context: CGContextRef
+
         public init(context: CGContextRef) {
             self.context = context
         }
-        
+
         public func fill(color: UIColor?) {
             CGContextSetFillColorWithColor(context, color?.CGColor)
+        }
+
+        public func drawrLinerGradient(start: CGPoint, end: CGPoint, gradient: Gradient) {
+            do {
+                let gradientRef = try gradient.createGradient()
+                CGContextDrawLinearGradient(context, gradientRef, start, end, CGGradientDrawingOptions.DrawsAfterEndLocation)
+            } catch SketchError.ResourceCreationFail(let functionName){
+                print("Failed to create resource on: \(functionName)")
+            } catch {
+
+            }
         }
         
         public func stroke(color: UIColor?) {
